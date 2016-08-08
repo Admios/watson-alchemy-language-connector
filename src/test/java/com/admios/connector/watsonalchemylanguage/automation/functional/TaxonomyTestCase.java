3af772bdd97bfee1eb2b5a9543272fe324fe966a
Enyert Vinas <enyert@Enyerts-MacBook-Pro.local>
2016-08-08 09:39:28 -0500

    Adding some tests

--- src/test/java/com/admios/connector/watsonalchemylanguage/automation/functional/TaxonomyTestCase.java
+++ src/test/java/com/admios/connector/watsonalchemylanguage/automation/functional/TaxonomyTestCase.java
@@ -1,5 +1,23 @@
 package com.admios.connector.watsonalchemylanguage.automation.functional;
 
-public class TaxonomyTestCase {
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.mule.tools.devkit.ctf.junit.AbstractTestCase;
 
+import com.admios.connector.watsonalchemylanguage.WatsonAlchemyLanguageConnector;
+import com.admios.connector.watsonalchemylanguage.automation.global.Constants;
+import com.ibm.watson.developer_cloud.alchemy.v1.model.Taxonomies;
+
+public class TaxonomyTestCase extends AbstractTestCase<WatsonAlchemyLanguageConnector> {
+	
+	public TaxonomyTestCase() {
+		super(WatsonAlchemyLanguageConnector.class);
+	}
+	
+	@Test
+	public void testWithURL() {
+		Taxonomies taxonomies = getConnector().getTaxonomies(Constants.GITHUB_URL_BLOG, null, null, null, null);
+	}
+	
+	
 }
