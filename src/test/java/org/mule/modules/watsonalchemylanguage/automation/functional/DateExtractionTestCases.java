fad543e95e1fb47d9bdda83fbaf2d9837a2d924c
Gianluigi Pierini <gluigip@hotmail.com>
2016-08-31 15:49:43 -0500

    Fixed the test DateExtractionTestCases (#34)

--- src/test/java/org/mule/modules/watsonalchemylanguage/automation/functional/DateExtractionTestCases.java
+++ src/test/java/org/mule/modules/watsonalchemylanguage/automation/functional/DateExtractionTestCases.java
@@ -6,35 +6,43 @@ package org.mule.modules.watsonalchemylanguage.automation.functional;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 import org.junit.Test;
 
 import com.ibm.watson.developer_cloud.alchemy.v1.model.Dates;
 
 public class DateExtractionTestCases extends AbstractTestCases {
-	
+
+	private DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss a");
+
 	@Test
 	public void getDatesUsingText() {
-		Dates dates = getConnector().dateExtraction("The person was found alive on November 25, 2014 at a ranch on the outskirts of city light.", null, null);
-		
+
+		Dates dates = getConnector().dateExtraction(
+				"The person was found alive on November 25, 2014 at a ranch on the outskirts of city light.",
+				null, null);
+
 		assertNotNull(dates);
 		assertEquals("english", dates.getLanguage());
 		assertEquals(1, dates.getTotalTransactions().intValue());
 		assertEquals(1, dates.getDates().size());
-		assertEquals("Tue Nov 25 00:00:00 EST 2014", dates.getDates().get(0).getDate().toString());
-		assertEquals("november 25, 2014 at", dates.getDates().get(0).getText());
+		assertEquals("25/11/2014 12:00:00 AM", formatter.format(dates.getDates().get(0).getDate()));
 	}
-	
+
 	@Test
 	public void getDatesUsingHTML() {
-		Dates dates = getConnector().dateExtraction("<html><body><h1>Finally found it!!!</h1><p>The Person was found alive on November 10, 2014 in Marsella.</p><p>The last time was on a party was October 31, 2014 at a ranch outside the City Lights.</p></body></html>", null, true);
-		
+		Dates dates = getConnector()
+				.dateExtraction("<html><body><h1>Finally found it!!!</h1><p>The Person was found alive on November 10,"
+						+ " 2014 in Marsella.</p><p>The last time was on a party was October 31, 2014 at a ranch outside the City Lights.</p></body></html>",
+						null, true);
+
 		assertNotNull(dates);
 		assertEquals("english", dates.getLanguage());
 		assertEquals(1, dates.getTotalTransactions().intValue());
 		assertEquals(2, dates.getDates().size());
-		assertEquals("Mon Nov 10 00:00:00 EST 2014", dates.getDates().get(0).getDate().toString());
-		assertEquals("november 10, 2014", dates.getDates().get(0).getText());
-		assertEquals("Fri Oct 31 00:00:00 EST 2014", dates.getDates().get(1).getDate().toString());
-		assertEquals("october 31, 2014 at", dates.getDates().get(1).getText());
+		assertEquals("10/11/2014 12:00:00 AM", formatter.format(dates.getDates().get(0).getDate()));
+		assertEquals("31/10/2014 12:00:00 AM", formatter.format(dates.getDates().get(1).getDate()));
 	}
 }
