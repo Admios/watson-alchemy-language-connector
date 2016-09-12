4dbb6d2e11110f8169445d28ac9027cb6e197ebf
Gianluigi Pierini <gluigip@hotmail.com>
2016-09-12 14:08:28 -0500

    Updated the headers (#58)

--- doc/apidocs/javadocs/javadoc.sh
+++ doc/apidocs/javadocs/javadoc.sh
@@ -1,5 +1,5 @@
 #
-# (c) Copyright 2016 Admios. The software in this package is published under the terms of the CPAL v1.0 license, a copy of which has been included with this distribution in the LICENSE.md file.
+# (c) Copyright 2016 Admios. The software in this package is published under the terms of the Apache License Version 2.0, a copy of which has been included with this distribution in the LICENSE.md file.
 #
 
 /Library/Java/JavaVirtualMachines/jdk1.8.0_65.jdk/Contents/Home/jre/../bin/javadoc @options @packages
